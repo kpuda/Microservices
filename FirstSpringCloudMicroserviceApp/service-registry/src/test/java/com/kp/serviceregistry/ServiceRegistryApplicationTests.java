a1e34b556d1903ec9d211eecf8c805a9ed536258
kpuda <kpuda.contact@gmail.com>
2023-01-07 16:54:34 +0100

    removing tests

--- FirstSpringCloudMicroserviceApp/service-registry/src/test/java/com/kp/serviceregistry/ServiceRegistryApplicationTests.java
+++ FirstSpringCloudMicroserviceApp/service-registry/src/test/java/com/kp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -3,7 +3,7 @@ package com.kp.serviceregistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServiceRegistryApplicationTests {
 
 	@Test
