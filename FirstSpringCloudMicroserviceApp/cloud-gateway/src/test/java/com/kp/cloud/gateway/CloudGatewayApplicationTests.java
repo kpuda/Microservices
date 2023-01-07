1044f51cfcb13c5f97567cc5918407eee37c56c1
kpuda <kpuda.contact@gmail.com>
2023-01-07 16:54:16 +0100

    adding methods to predicates

--- FirstSpringCloudMicroserviceApp/cloud-gateway/src/test/java/com/kp/cloud/gateway/CloudGatewayApplicationTests.java
+++ FirstSpringCloudMicroserviceApp/cloud-gateway/src/test/java/com/kp/cloud/gateway/CloudGatewayApplicationTests.java
@@ -3,7 +3,6 @@ package com.kp.cloud.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CloudGatewayApplicationTests {
 
 	@Test
