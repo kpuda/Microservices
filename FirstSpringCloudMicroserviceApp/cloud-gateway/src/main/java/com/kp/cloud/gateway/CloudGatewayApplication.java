7a41b03030fb2cf39ae0cfb81022f0c9406b896b
kpuda <kpuda.contact@gmail.com>
2023-01-07 09:51:25 +0100

    adding circuit breaker

--- FirstSpringCloudMicroserviceApp/cloud-gateway/src/main/java/com/kp/cloud/gateway/CloudGatewayApplication.java
+++ FirstSpringCloudMicroserviceApp/cloud-gateway/src/main/java/com/kp/cloud/gateway/CloudGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
 
 }
