c2497660d4d6438f2e78d6a20ed8afbcf16f4c2e
k.puda <krzysztof.puda@ing.com>
2023-01-09 10:53:23 +0100

    removing unused import

--- FirstSpringCloudMicroserviceApp/5-inventory-service/src/main/java/com/kp/inventory/service/repositories/InventoryRepository.java
+++ FirstSpringCloudMicroserviceApp/5-inventory-service/src/main/java/com/kp/inventory/service/repositories/InventoryRepository.java
@@ -4,7 +4,6 @@ import com.kp.inventory.service.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
