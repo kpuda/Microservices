bf76bbb721222be0abd4c59dcddd11eddcfcd4c5
k.puda <krzysztof.puda@ing.com>
2023-01-11 10:27:03 +0100

    adding new services

---
+++ 3-accountManaagement/src/main/java/com/kp/account/manaagement/controllers/AccountController.java
@@ -0,0 +1,15 @@
+package com.kp.account.manaagement.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Working...";
+    }
+}
---
+++ 4-users-microservice/src/test/java/com/kp/users/microservice/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.users.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
