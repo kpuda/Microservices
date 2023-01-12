149e5466a457cab0e78db00894e98555ee2e8884
k.puda <krzysztof.puda@ing.com>
2023-01-12 14:24:43 +0100

    adding some global pre and post filters to api gateway

--- 2-api-gateway/src/main/java/com/kp/api/gateway/filters/GlobalFiltersConfig.java
+++ 2-api-gateway/src/main/java/com/kp/api/gateway/filters/GlobalFiltersConfig.java
@@ -1,2 +1,47 @@
-package com.kp.api.gateway.filters;public class GlobalFiltersConfig {
+package com.kp.api.gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class GlobalFiltersConfig {
+
+    @Bean
+    @Order(1)
+    public GlobalFilter secondPreFilter() {
+        return (exchange, chain) -> {
+            log.info("My second global preFilter is executed...");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("My third global postFilter is executed...");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public GlobalFilter thirdPreFilter() {
+        return (exchange, chain) -> {
+            log.info("My third global preFilter is executed...");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("My second global postFilter is executed...");
+            }));
+        };
+    }
+
+
+    @Bean
+    @Order(3)
+    public GlobalFilter fourthPreFilter() {
+        return (exchange, chain) -> {
+            log.info("My fourth global preFilter is executed...");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("My first global postFilter is executed...");
+            }));
+        };
+    }
 }
--- 2-api-gateway/src/main/java/com/kp/api/gateway/filters/MyPostFilter.java
+++ 2-api-gateway/src/main/java/com/kp/api/gateway/filters/MyPostFilter.java
@@ -1,2 +1,27 @@
-package com.kp.api.gateway.filters;public class MyPostFilter {
+package com.kp.api.gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class MyPostFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            log.info("My last global post filter executed...");
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        //This filter will be executed last because of order 0. When You use PostFilters the order is from highest to lowest
+        return 0;
+    }
 }
