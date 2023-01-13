fd9c02a7a1155e1979d98b328d34e6fcef513fb3
k.puda <krzysztof.puda@ing.com>
2023-01-13 14:55:02 +0100

    moving properties to cloud-properties

--- 5-config-server/src/main/java/com/kp/config/server/ConfigServer.java
+++ 5-config-server/src/main/java/com/kp/config/server/ConfigServer.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class Application {
+public class ConfigServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ConfigServer.class, args);
     }
 
 }
