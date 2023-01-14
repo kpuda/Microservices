5fc74072bc9990249d5c99f47ea4d973ed4757da
k.puda <krzysztof.puda@ing.com>
2023-01-14 08:46:18 +0100

    adding mysql

--- 4-users-microservice/src/main/java/com/kp/users/microservice/controllers/UserController.java
+++ 4-users-microservice/src/main/java/com/kp/users/microservice/controllers/UserController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users/")
+@RequestMapping("/users")
 public class UserController {
 
     private final Environment env;
@@ -24,10 +23,10 @@ public class UserController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "working on port: " + env.getProperty("local.server.port")+", with token secret: "+env.getProperty("jwt.secret");
+        return "working on port: " + env.getProperty("local.server.port") + ", with token secret: " + env.getProperty("jwt.secret");
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel model) {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserDto userDto = modelMapper.map(model, UserDto.class);
