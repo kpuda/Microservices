9f09a5e707fcd46c1b2f699b75f45c450cc90792
k.puda <krzysztof.puda@ing.com>
2023-01-19 08:39:42 +0100

    changing dir

--- SpringCloud/4-users-microservice/src/main/java/com/kp/users/microservice/service/security/CustomAuthenticationProvider.java
+++ SpringCloud/4-users-microservice/src/main/java/com/kp/users/microservice/service/security/CustomAuthenticationProvider.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
